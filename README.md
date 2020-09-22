a22cf73586c6c45f3944084edb3ed774cab84c04
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-09-22 20:59:51 -0300

    first lesson

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Assembly-study
+
+in linux
+//command to compile with nasm 
+nasm -f elf64 hello.asm
+//creating executable
+ld -s -o hello hello.o
+ 
+
+this send anything at kernel : int 0x80
---
+++ hello-world/main.asm
@@ -0,0 +1,23 @@
+;this is a comment
+
+;this program is a simple hello world in assembly
+
+section .data:
+;this section is used to declaration of variables
+    msg db 'Hello, world', 0xa
+    len equ $ - msg
+
+section .text:
+;this section is where we write the program
+global _start:
+
+_start:
+    mov edx, len
+    mov eex, msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+; output
+    mov eax, 1
+    mov edx, 0
+    int 0x80
